e590cf8b34947ca95e9730e3a3e398e1f57376c1
veronikaKochugova <veronika.kochugova@gmail.com>
2018-12-11 11:23:22 +0300

    fixed NotConvex in Curs

--- curs/NotConvex.py
+++ curs/NotConvex.py
@@ -6,27 +6,7 @@ from curs.constants import *
 import numpy
 
 CUBE_WIDTH = 0.2
-W = 0.2
-
-# V = [[+CUBE_WIDTH, +CUBE_WIDTH, +CUBE_WIDTH],
-#      [+CUBE_WIDTH, +CUBE_WIDTH, -CUBE_WIDTH],
-#      [+CUBE_WIDTH, -CUBE_WIDTH, -CUBE_WIDTH],
-#      [-CUBE_WIDTH, -CUBE_WIDTH, -CUBE_WIDTH],
-#      [-CUBE_WIDTH, -CUBE_WIDTH, +CUBE_WIDTH],
-#      [-CUBE_WIDTH, +CUBE_WIDTH, +CUBE_WIDTH],
-#      [+CUBE_WIDTH, -CUBE_WIDTH, +CUBE_WIDTH],
-#      [-CUBE_WIDTH, +CUBE_WIDTH, -CUBE_WIDTH]]
-#
-# PLANES = [
-#     (V[2], V[3], V[7], V[1]),  # 1
-#     (V[6], V[2], V[1], V[0]),  # 2
-#     (V[4], V[6], V[0], V[5]),  # 3
-#     (V[3], V[4], V[5], V[7]),  # 4
-#     (V[6], V[4], V[3], V[2]),  # 5
-#     (V[5], V[0], V[1], V[7]),  # 6
-# ]
-#
-# COORDS = ([0, 0], [CUBE_WIDTH, 0], [CUBE_WIDTH, CUBE_WIDTH], [0, CUBE_WIDTH])
+W = 0.5
 
 V = [[-W, +W, +W],  # 0
      [-W, +W, -W],  # 1
@@ -38,21 +18,34 @@ V = [[-W, +W, +W],  # 0
      [+W, -W, -W],  # 7
      [+W, -W, +W]]  # 8
 
-PLANES = [
-    # (V[0], V[3], V[8], V[5]),  # 1
-    # (V[0], V[1], V[6], V[5]),  # 2
-    # (V[1], V[2], V[7], V[6]),  # 3
-    # (V[2], V[3], V[8], V[7]),  # 4
+T_PLANES = [
+    (V[0], V[1], V[4]),
+    (V[1], V[6], V[4]),
+    (V[6], V[5], V[4]),
+    (V[5], V[0], V[4]),
     #
-    (V[0], V[1], V[4]),  # 5
-    (V[0], V[3], V[4]),  # 6
-    (V[3], V[2], V[4]),  # 7
-    (V[1], V[2], V[4]),  # 8
+    (V[3], V[2], V[4]),
+    (V[2], V[7], V[4]),
+    (V[7], V[8], V[4]),
+    (V[8], V[3], V[4]),
     #
-    (V[7], V[8], V[4]),  # 9
-    (V[6], V[7], V[4]),  # 10
-    (V[5], V[6], V[4]),  # 11
-    (V[8], V[5], V[4])  # 12
+    #
+    (V[0], V[1], V[4]),
+    (V[1], V[2], V[4]),
+    (V[2], V[3], V[4]),
+    (V[3], V[0], V[4]),
+    #
+    (V[7], V[8], V[4]),
+    (V[6], V[7], V[4]),
+    (V[5], V[6], V[4]),
+    (V[8], V[5], V[4])
+]
+
+Q_PLANES = [
+    (V[0], V[3], V[8], V[5]),  # 1
+    (V[0], V[1], V[6], V[5]),  # 2
+    (V[1], V[2], V[7], V[6]),  # 3
+    (V[2], V[3], V[8], V[7]),  # 4
 ]
 
 
@@ -69,17 +62,27 @@ class NotConvex:
         glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, WHITE)
         # glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, WHITE_L)
 
-        glTranslatef(0, -CUBE_WIDTH, 0)
+        glTranslatef(0, -W, 0)
         glTranslatef(self.x, self.y, self.z)
         glRotatef(self.t, 0, 1, 0)
         glRotatef(-(self.t * 1.3), 0, 0, 1)
 
-        glBegin(GL_QUADS)
-        for plane in PLANES:
+        # glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        glBegin(GL_TRIANGLES)
+        for plane in T_PLANES:
             for vertex in plane:
+                print(vertex)
                 glNormal3f(vertex[0], vertex[1], vertex[2])
                 glVertex3f(vertex[0], vertex[1], vertex[2])
         glEnd()
+
+        # glBegin(GL_QUADS)
+        # for plane in Q_PLANES:
+        #     for vertex in plane:
+        #         print(vertex)
+        #         glNormal3f(vertex[0], vertex[1], vertex[2])
+        #         glVertex3f(vertex[0], vertex[1], vertex[2])
+        # glEnd()
 
         # undo changes
         glTranslatef(-self.x, -self.y, -self.z)
